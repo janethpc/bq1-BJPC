09d63784478820657b24a8836065bb169815d200
Janeth Pérez <janeth_2124@hotmail.com>
2022-12-03 14:14:16 -0600

    tarjeta chef

--- BQ9/src/vistas/pages/meseroOrdenes.jsx
+++ BQ9/src/vistas/pages/meseroOrdenes.jsx
@@ -3,36 +3,79 @@ import { ProductList } from "../components/ProductList"
 
 export const MeseroOrdenes = () => {
 
-    return (
-        <>
-        <h1>Nuevo Pedido</h1>
-        <div class="row justify-content-between">
-        <div class="col-8">
+  return (
+    <>
+      <h1>Nuevo Pedido</h1>
+      <div class="row justify-content-between">
+        <div class="col-5">
           <h2>Menu</h2>
           <button className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  modal</button>
+            modal</button>
 
           <ModalnewOrder />
-          <hr/>
+          <hr />
 
-      <h2 className="btn">Desayuno</h2>
-      <hr/>
+          <h2 className="btn">Desayuno</h2>
+          <hr />
 
-      <ProductList type='Desayuno'/>
-      
-      <br/>
+          <ProductList type='Desayuno' />
 
-      <h3 className="btn">Almuerzo</h3>
-      <hr/>
-      
+          <br />
 
-      <ProductList type='almuerzo'/>
+          <h3 className="btn">Almuerzo</h3>
+          <hr />
+
+
+          <ProductList type='almuerzo' />
         </div>
-        <div class="col-4">
+
+        <div className="col-7">
           <h2>Resumen de la orden</h2>
+          <div className='card'>
+            <h5 className="card-header">
+              Orden:
+            </h5>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item ">
+                <input className="form-check-input" type="checkbox" id="checkboxNoLabel" />
+                <lebel className="form-check-label">An item</lebel>
+              </li>
+              <li className="list-group-item">A second item</li>
+              <li className="list-group-item">A third item</li>
+            </ul>
+            <div className="card-footer">
+              <p> Nombre del Mesero: <span>fulanito</span></p>
+              <a>hora de salida: </a>
+              <div className="text-end">
+                <button href="#" className="btn btn-primary">Entregar</button>
+              </div>
+            </div>
+          </div>
+<br/>
+          <div className='card'>
+            <h5 className="card-header">
+              Orden:
+            </h5>
+            <ul className="list-group list-group-flush">
+              <li className="list-group-item ">
+                <input className="form-check-input" type="checkbox" id="checkboxNoLabel" />
+                <lebel className="form-check-label">An item</lebel>
+              </li>
+              <li className="list-group-item">A second item</li>
+              <li className="list-group-item">A third item</li>
+            </ul>
+            <div className="card-footer">
+              <p> Nombre del Mesero: <span>fulanito</span></p>
+              <a>hora de salida: </a>
+              <div className="text-end">
+                <button href="#" className="btn btn-primary">Entregar</button>
+              </div>
+            </div>
+          </div>
+
         </div>
       </div>
-        </>
-        
-    )
+    </>
+
+  )
 }
